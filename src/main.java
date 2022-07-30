eb2acd02728d36c18f725dc9ce5cd45d5d17a6ab
kikuha21400 <kirill21400@gmail.com>
2022-07-30 23:13:24 +0300

    created basic interface

--- src/main.java
+++ src/main.java
@@ -1,6 +1,46 @@
+import java.io.*;
 
+    
 class Calculator{
+    String calcString;
+
+    Calculator(String calcString){
+	this.calcString = calcString;
+    }
+
+    public boolean isValid(){
+	return true;
+    }
+
+    public int calculate(){
+	return 0;
+    }
+
+    public String getCalcString(){
+	return new String(calcString);
+    }
+}
+
+class CalculatorDemo{
     public static void main (String[] args){
-	System.out.println("Hello, world");
+
+	Console con = System.console();
+	String demoStr;
+
+	if (con == null){
+	    System.out.println("No console available");
+	    return;
+	}
+
+	demoStr = con.readLine("Enter expression to calculate:\n");
+
+	Calculator calc = new Calculator(new String(demoStr));
+
+	if (calc.isValid()){
+	    con.printf("Result is %d\n", calc.calculate());
+	} else {
+	    System.out.println("Error while parsing");
+	}
+	
     }
 }
